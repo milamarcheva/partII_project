7efd79104954fd9a3ef40b13136b6b8fdaff13ff
Mila Marcheva <mila.m.marcheva@gmail.com>
2020-12-20 17:39:20 +0000

    added new packages

---
+++ analysis/concreteness.py
@@ -0,0 +1,3 @@
+# Create vocab from lexicon (set)
+# Encode tokens; OOV token
+# Look for token encoding in vocab
--- preprocessing/dataset_reshaping.py
+++ preprocessing/dataset_reshaping.py
@@ -4,11 +4,6 @@ hc = pd.read_csv(r'../data/hippoCorpusV2.csv')
 
 hc_trunc = hc[['AssignmentId', 'story', 'memType', 'mainEvent', 'summary']]
 hc_trunc.columns = ['id', 'story', 'label', 'event', 'summary']
+hc_trunc = hc_trunc[hc_trunc.label!='retold']
+hc_trunc.to_csv(r'../data/hc_trunc.csv', index=False)
 
-hc_Brysbaert = hc_trunc[['id', 'story', 'label']]
-hc_LIWC = hc_trunc[['id', 'story', 'label']]
-hc_GPT2 = hc_trunc.copy()
-
-hc_Brysbaert.to_csv(r'../data/hc_Brysbaert.csv', index=False)
-hc_LIWC.to_csv(r'../data/hc_LIWC.csv', index=False)
-hc_GPT2.to_csv(r'../data/hc_GPT2.csv', index=False)
