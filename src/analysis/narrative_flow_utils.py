e0eddcf2190f2254c52f2c7cd789f0496e0a8d72
Mila Marcheva <mila.m.marcheva@gmail.com>
2021-03-31 20:38:51 +0100

    update 31 march
     :wq!
    
     <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)

--- src/analysis/narrative_flow_utils.py
+++ src/analysis/narrative_flow_utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def get_narrative_flow(logprobs_bag, logprobs_chain, encoded_sentences_len):
-    n = len(logprobs_bag)
-    narrative_flow = [0] * n
-    for i in range(n):
-        sentences_len = np.array(encoded_sentences_len[i])
+    number_of_sentences = len(logprobs_bag)
+    narrative_flow = [0] * number_of_sentences
+    for i in range(number_of_sentences):
+        sentence_len = np.array(encoded_sentences_len[i])
         diff = np.array(logprobs_bag[i]) - np.array(logprobs_chain[i])
-        narrative_flow[i] = -(diff / sentences_len)
+        narrative_flow[i] = -(diff/sentence_len)
     return narrative_flow
